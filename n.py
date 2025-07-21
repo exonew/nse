476664bcc1c996c08d52f82a544cfd96a143cd41
exonew <jpqlps+211@gmail.com>
2025-07-21 22:23:36 +0530

    Update n.py

--- n.py
+++ n.py
@@ -4,6 +4,7 @@ from datetime import date
 today_str = date.today().isoformat()
 filename = f"data/{today_str}.json"
 positions = nsefetch('https://www.nseindia.com/api/etf')
-with open(filename, "w+") as f:
-    json.dump(positions, f, indent=4)
-print(f"Data saved to {filename}")
+try:
+    with open(filename, "w+") as f:
+        json.dump(positions, f, indent=4)
+    print(f"Data saved to {filename}")
