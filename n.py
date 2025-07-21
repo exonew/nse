bf27896b0c3be871235fe3c37d02b5488d270de0
exonew <jpqlps+211@gmail.com>
2025-07-21 20:42:53 +0530

    Create n.py

---
+++ n.py
@@ -0,0 +1,9 @@
+from nsepython import *
+import json
+from datetime import date
+today_str = date.today().isoformat()
+filename = f"data/{today_str}.json"
+positions = nsefetch('https://www.nseindia.com/api/etf')
+with open(filename, "w") as f:
+    json.dump(positions, f, indent=4)
+print(f"Data saved to {filename}")
